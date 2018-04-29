0885b770d1f564e6dfa7f81b7449a27690b4332b
Prerna <sainiprerna11@gmail.com>
2018-04-30 02:01:42 +0530

    email Auth

--- app/src/main/java/com/example/apoorva/gpandroid/loginOrSignup.java
+++ app/src/main/java/com/example/apoorva/gpandroid/loginOrSignup.java
@@ -2,17 +2,31 @@ package com.example.apoorva.gpandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class loginOrSignup extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private EditText email, pass;
+    private static final String TAG = "FirebaseEmailPassword";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_or_signup);
+        email = (EditText) findViewById(R.id.sign_email);
+        pass = (EditText) findViewById(R.id.sign_pass);
         /*
         *************************************FIREBASE CODE*******************************************
          */
@@ -22,16 +36,67 @@ public class loginOrSignup extends AppCompatActivity {
             startActivity(new Intent(loginOrSignup.this, exit.class));
             finish();
         }
+    }
          /*
           ********************************************************CODE ENDS HERE**********************************************
          */
-        findViewById(R.id.sign_inButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(loginOrSignup.this,signup.class));
-            }
-        });
+  public void sign_up(View view)
+    {
+     startActivity(new Intent(loginOrSignup.this, signup.class));
     }
+
+    public void login(View view)
+    {
+        signIn(email.getText().toString(), pass.getText().toString());
+    }
+    private void signIn(String email, String password) {
+        Log.e(TAG, "signIn:" + email);
+        if (!validateForm(email, password)) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.e(TAG, "signIn: Success!");
+
+                            // update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(loginOrSignup.this, exit.class));
+                        } else {
+                            Log.e(TAG, "signIn: Fail!", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed!", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        if (!task.isSuccessful()) {
+
+                        }
+                    }
+                });
+    }
+    private boolean validateForm(String email, String password) {
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
     public void signphone(View view)
     {
         startActivity(new Intent(loginOrSignup.this, phone.class));
--- app/src/main/java/com/example/apoorva/gpandroid/signup.java
+++ app/src/main/java/com/example/apoorva/gpandroid/signup.java
@@ -16,58 +16,117 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class signup extends AppCompatActivity {
+public class signup extends AppCompatActivity implements
+        View.OnClickListener {
     private static final String TAG = "PhoneAuth";
     private EditText mEmail;
     private EditText mPassword;
     private FirebaseAuth fbAuth;
     private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+    private EditText email, name, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        email = (EditText)findViewById(R.id.e1);
+        name = (EditText)findViewById(R.id.name);
+        pass = (EditText)findViewById(R.id.e2);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
 
         /*
         *************************************FIREBASE CODE*******************************************
          */
-        fbAuth = FirebaseAuth.getInstance();
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-        //Get the data
-        mEmail = findViewById(R.id.e1);
-        mPassword = findViewById(R.id.e2);
-        mDatabase = FirebaseDatabase.getInstance().getReference("users");
-        //Click on the button
-        findViewById(R.id.sign_in).setOnClickListener(new View.OnClickListener() {
-                                                          @Override
-                                                          public void onClick(View view) {
-                                                              //Check whether the data is accurate
-                                                              if (!checkData()) {
-                                                                  Log.d(TAG, "Not correct details");
-                                                                  return;
-                                                              }
-                                                              //Create a user and sign in via firebase Auth
-                                                              String email = mEmail.getText().toString();
-                                                              String password = mPassword.getText().toString();
-                                                              fbAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(signup.this, new OnCompleteListener<AuthResult>() {
-                                                                  @Override
-                                                                  public void onComplete(@NonNull Task<AuthResult> task) {
-                                                                      if (!task.isSuccessful()) {
-                                                                          Toast.makeText(signup.this, "Authentication failed." + task.getException(),
-                                                                                  Toast.LENGTH_SHORT).show();
-                                                                      }
-                                                                  }
-                                                              });
-                                                          }
-                                                      });
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+         startActivity(new Intent(signup.this, exit.class));
+    }
 
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
 
+        if (i == R.id.btn_email_create_account) {
+            createAccount(email.getText().toString(), pass.getText().toString());
+        } else if (i == R.id.btn_verify_email) {
+            sendEmailVerification();
+        }
+    }
+    private void createAccount(String email, String password) {
+        Log.e(TAG, "createAccount:" + email);
+        if (!validateForm(email, password)) {
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.e(TAG, "createAccount: Success!");
+
+                            // update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(signup.this, exit.class));
+                        } else {
+                            Log.e(TAG, "createAccount: Fail!", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private void sendEmailVerification() {
+        // Disable Verify Email button
+        findViewById(R.id.btn_verify_email).setEnabled(false);
+
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Re-enable Verify Email button
+                        findViewById(R.id.btn_verify_email).setEnabled(true);
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Verification email sent to " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification failed!", task.getException());
+                            Toast.makeText(getApplicationContext(), "Failed to send verification email.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    private boolean validateForm(String email, String password) {
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
          /*
     ********************************************************CODE ENDS HERE**********************************************
      */
-    }
 
     //Firebase sign in success function
     private void onAuthSucess(FirebaseUser user){
